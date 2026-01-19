5df96a9a7279809c5edfbb8e680ee369e011d1f8
mlk <mlkmalkalzryqy@gmail.com>
2026-01-19 13:04:14 +0300

    lab7

---
+++ src/Stack/Stack.java
@@ -0,0 +1,16 @@
+
+package Stack;
+
+public interface Stack<E> {
+    public boolean isEmpty();
+    public int size();
+    public void push(E data);
+    public E top();
+    public E pop();
+
+
+
+
+
+
+}
